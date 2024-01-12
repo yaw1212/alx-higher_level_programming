1e332c4114b898d16844ac1987b393b1f66662fc
Yaw1212 <baronchase6@gmail.com>
2024-01-11 22:13:51 -0800

    Implemented roman_to_int function to convert a Roman numeral to an integer. the function handles Roman numeral inputs within the range of 1 to 3999 and returns the corespondind integer. It also includes handling for non-string or None inputs, returning 0 in such cases.

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    if not isinstance(roman_string, str) or not roman_string:
+        return (0)
+    ro_dic = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    result = 0
+    for i in range(len(roman_string)):
+        if i > 0 and ro_dic[roman_string[i]] > ro_dic[roman_string[i - 1]]:
+            result += ro_dic[roman_string[i]] - 2 * ro_dic[roman_string[i - 1]]
+        else:
+            result += ro_dic[roman_string[i]]
+
