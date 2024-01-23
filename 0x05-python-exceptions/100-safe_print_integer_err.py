3024ea0cd3009faea660602b9df8ec58c3e2abfe
Yaw1212 <baronchase6@gmail.com>
2024-01-22 16:42:48 -0800

    Add print_integer function to display the provided integer value

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python3
-import sys
-
 def safe_print_integer_err(value):
-    is_intgr = True
+    import sys
     try:
         print("{:d}".format(value))
-    except (ValueError, TypeError):
-        print("Exception: {}".format(sys.exc_info()[1]), file=sys.stderr)
-        is_intgr = False
-    return (is_intgr)
+        return True
+    except (ValueError, TypeError) as e:
+        print("Exception: "+str(e), file=sys.stderr)
+        return False
