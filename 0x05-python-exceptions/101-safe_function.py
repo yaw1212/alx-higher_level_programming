f94438ced803929a201dc3231a9f8062af929980
Yaw1212 <baronchase6@gmail.com>
2024-01-22 16:54:30 -0800

    Implement safe_function to execute a function with error handling

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -1,9 +1,13 @@
 #!/usr/bin/python3
+from __future__ import print_function
 import sys
+
+
 def safe_function(fct, *args):
     try:
         res = fct(*args)
-        return (res)
-    except Exception as er:
-        print("Exception: {}".format(er), file=sys.stderr)
+    except Exception as e:
+        print("Exception: {}".format(e), file=sys.stderr)
         return None
+    else:
+        return res
