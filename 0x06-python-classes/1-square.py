d8630c2efc52f30876754a935daf27bdf5610b3c
Yaw1212 <baronchase6@gmail.com>
2024-01-23 16:51:20 -0800

    Implemented the Square class according to the specifications outlined in the file 0-square.py, providing the foundational structure for representing and handling square geometries

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Square module."""
+
+
+class Square:
+    """Defines a square class."""
+
+    def __init__(self, size):
+        """Constructor-Initialzation.
+        Args:
+             * size: length of side of square.
+        """
+        self.__size = size
