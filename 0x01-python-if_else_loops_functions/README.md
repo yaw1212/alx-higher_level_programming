536ec62ce27fc1df5a14f9c1fb54c645258d289a
Yaw1212 <baronchase6@gmail.com>
2024-02-06 19:10:34 -0800

    Implement function to insert text into file after specific string

---
+++ 0x01-python-if_else_loops_functions/README.md
@@ -0,0 +1,4 @@
+# Python Basics: If-Else, Loops, and Functions
+
+Welcome to the Python Basics file! This guide provides an introduction to fundamental concepts in Python, including conditional statements, loops, and functions.
+
--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -1,18 +1,24 @@
 #!/usr/bin/python3
-"""Module define the "append after" function """
+"""
+Module for append_after function.
+"""
 
 
 def append_after(filename="", search_string="", new_string=""):
-    """appends 'new_string' after a line containing
-    'search_string' in 'filename' """
-    with open(filename, 'r', encoding='utf-8') as f:
-        l_list = []
-        while True:
-            l = f.readline()
-            if l == "":
-                break
-            l_list.append(l)
-            if search_string in l:
-                l_list.append(new_string)
-    with open(filename, 'w', encoding='utf-8') as f:
-        f.writelines(l_list)
+    """
+    Inserts a line of text after each line containing a specific string.
+
+    Args:
+        filename (str): The name of the file.
+        search_string (str): The string to search for in each line.
+        new_string (str): The string to append after the lines containing
+        search_string.
+    """
+    with open(filename, mode='r', encoding='utf-8') as file:
+        lines = file.readlines()
+
+    with open(filename, mode='w', encoding='utf-8') as file:
+        for line in lines:
+            file.write(line)
+            if search_string in line:
+                file.write(new_string)
