d7f618fd76304aab0eb5441ea91e89f7aeff0a59
Yaw1212 <baronchase6@gmail.com>
2024-03-27 09:24:23 -0700

    README update

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-''' authur abdellah'''
-value = 0
 for i in range(ord('z'), ord('a') - 1, -1):
-    print("{}".format(chr(i + value)), end="")
-    value = 32 if i == 0 else 0
+    if i % 2 == 0:
+        diff = 0
+    else:
+        diff = 32
+    print('{}'.format(chr(i - diff)), end='')
